169dbcd4af16194c99f5ea73fb5c83689ea9aab8
stkddks <stkddks@naver.com>
2021-02-10 03:58:52 +0900

    presentation(web) layer CRUD

---
+++ springMVC/src/main/java/org/zerock/controllers/BoardController.java
@@ -0,0 +1,73 @@
+package org.zerock.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.domain.BoardVO;
+import org.zerock.service.BoardService;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Controller // @Controller 어노테이션이 붙게되면 이제 BoardController가 서블릿이 되는 것이다. // 서블릿에 대한 설정은
+			// servlet-context.xml에 있다.
+@Log4j // 생성자함수를 위한 어노체이션
+@RequestMapping("/board/*") // mapping은 변환을 뜻한다. /board/* 즉, 모든파일을 변환하겠다 라는 의미. (제어를 하겠다)
+@AllArgsConstructor
+// @AllArgsConstructor를 이용해서 생성자를 만들고 자동으로 주입하도록 합니다. 
+// (만일 생성자를 만들지 않을 경우에는 @Setter(onMethod_ = {@Autowired})를 이용해서 처리합니다. 
+
+public class BoardController {
+
+	private BoardService service;
+
+	@GetMapping("/list") // 4점대 버전에서 get방식을 사용하는 법
+	public void list(Model model) { // 목록에 대한 처리와 테스트
+		log.info("list");
+		model.addAttribute("list", service.getList());
+	}
+
+	@PostMapping("/register")
+	public String register(BoardVO board, RedirectAttributes rttr) {
+
+		log.info("register: " + board);
+
+		service.register(board);
+
+		rttr.addFlashAttribute("result", board.getBno());
+
+		return "redirect:/board/list";
+	}
+
+	@GetMapping("/get")
+	public void get(@RequestParam("bno") Long bno, Model model) {
+
+		log.info("/get  ");
+		model.addAttribute("board", service.get(bno));
+	}
+
+	@PostMapping("/modify")
+	public String modify(BoardVO board, RedirectAttributes rttr) {
+		log.info("modify:" + board);
+
+		if (service.modify(board)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		return "redirect:/board/list";
+	}
+
+	@PostMapping("/remove")
+	public String remove(@RequestParam("bno") Long bno, RedirectAttributes rttr) {
+
+		log.info("remove..." + bno);
+		if (service.remove(bno)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		return "redirect:/board/list";
+	}
+
+}
--- springMVC/src/test/java/org/zerock/controllers/BoardControllerTests.java
+++ springMVC/src/test/java/org/zerock/controllers/BoardControllerTests.java
@@ -19,6 +19,8 @@ import lombok.extern.log4j.Log4j;
 
 // Test for Controller
 @WebAppConfiguration
+// @WebAppConfiguration은 servlet의 ServletContext를 이용하기 위해서
+// 스프링에서는 WebApplicationContext라는 존재를 이용하기 위해서입니다.
 
 @ContextConfiguration({ "file:src/main/webapp/WEB-INF/spring/root-context.xml",
 		"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml" })
@@ -28,7 +30,7 @@ public class BoardControllerTests {
 	@Setter(onMethod_ = { @Autowired })
 	private WebApplicationContext ctx;
 
-	private MockMvc mockMvc;
+	private MockMvc mockMvc; // moc의 뜻은 가짜라는 의미. 즉, 테스트용으로 쓰겠다.
 
 	@Before
 	public void setup() {
@@ -40,4 +42,52 @@ public class BoardControllerTests {
 		log.info(
 				mockMvc.perform(MockMvcRequestBuilders.get("/board/list")).andReturn().getModelAndView().getModelMap());
 	}
+
+	@Test
+	public void testRegister() throws Exception {
+
+		String resultPage = mockMvc
+				.perform(MockMvcRequestBuilders.post("/board/register").param("title", "테스트 새글 제목")
+						.param("content", "테스트 새글 내용").param("writer", "user00"))
+				.andReturn().getModelAndView().getViewName();
+		log.info(resultPage);
+
+	}
+
+	@Test
+	public void tetGet() throws Exception {
+		log.info(mockMvc.perform(MockMvcRequestBuilders.get("/board/get").param("bno", "2")).andReturn()
+				.getModelAndView().getModelMap());
+	}
+
+	@Test
+	public void testModify() throws Exception {
+		String resultPage = mockMvc
+				.perform(MockMvcRequestBuilders.post("/board/modify").param("bno", "1").param("title", "update new title")
+						.param("content", "update new content").param("writer", "user11"))
+				.andReturn().getModelAndView().getViewName();
+		log.info(resultPage);
+
+	}
+
+	@Test
+	public void testRemove() throws Exception {
+		// 삭제전 데이터베이스에 게시물 번호 확인할 것
+		String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/remove").param("bno", "6")).andReturn()
+				.getModelAndView().getViewName();
+		log.info(resultPage);
+	}
+
 }
+
+
+
+/*
+경우에 따라서는 Controller에 대한 테스트 코드를 작성하는 것에 대해서 거부감을 가지는 경우도 많습니다. 대부분은 일정에 여유가 없다는
+이유로 테스트를 작성하지 않는 경우가 많은데 프로젝트를 진행하는 멤버들의 경험치가 낮을수록 테스트를 먼저 진행하는 습관을 가지는 것이
+좋습니다. 반복적으로 입력과 수정, WAS의 재시작시간을 고려해보면 Controller 에 대한 테스트를 진행하는 선택이 더 빠른 개발의 결과를 
+낳는 경우가 많습니다. 유념하세요.
+*/
+
+
+
